2f345dded51be8fdd6e9dfb95fa88fd2fd2eacb1
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-10-11 17:04:51 +0900

    practice4

--- main.c
+++ main.c
@@ -1,35 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int sumtwo(int a, int b)
-{
-	int sum = a + b;
-	return(sum);
-}
-
-int square(int n)
-{
-	return (n*n);
-}
-
-int get_max(int x, int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
-}
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int square(int a)
+{
+	return a*a;
+}
 int main(void)
 {	
-	int res_sum, res_square, res_get_max;
+	int a = 2;
 	
-	res_sum = sumtwo(2,3);
-	res_square = square(5);
-	res_get_max = get_max(10, -20);
+	a = square(a);
 	
-	printf("%i %i %i", res_sum, res_square, res_get_max);
+	printf("a = %i\n", a);
 	
 	return 0;
 }
