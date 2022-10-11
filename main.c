ed29f7020f38c677564fc3a2d5255f03e475d5c4
KSY173 <112866329+KSY173@users.noreply.github.com>
2022-10-11 17:00:19 +0900

    practice3

--- main.c
+++ main.c
@@ -1,19 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void func(int x)
+int sumtwo(int a, int b)
 {
-	printf("func x is at %p\n", &x);
+	int sum = a + b;
+	return(sum);
 }
 
+int square(int n)
+{
+	return (n*n);
+}
+
+int get_max(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+}
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void)
-{
-	int x;
+{	
+	int res_sum, res_square, res_get_max;
 	
-	printf("main x is at %p\n", &x);
-	func(x);
+	res_sum = sumtwo(2,3);
+	res_square = square(5);
+	res_get_max = get_max(10, -20);
+	
+	printf("%i %i %i", res_sum, res_square, res_get_max);
 	
 	return 0;
 }
